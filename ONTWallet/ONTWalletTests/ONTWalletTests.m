93b73af8b45a549728dd7ef22e44fee374d6b902
zhangyutao <zhangyutao2014@163.com>
2018-07-13 10:31:00 +0800

    Update

---
+++ ONTWallet/ONTWalletTests/ONTWalletTests.m
@@ -0,0 +1,39 @@
+//
+//  ONTWalletTests.m
+//  ONTWalletTests
+//
+//  Created by zhangyutao on 2018/7/13.
+//  Copyright © 2018年 zhangyutao. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ONTWalletTests : XCTestCase
+
+@end
+
+@implementation ONTWalletTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
